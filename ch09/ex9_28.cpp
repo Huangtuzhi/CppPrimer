ae3b438a178a6f602d3896a4b2b56637804f9390
Huangtuzhi <heying1991@gmail.com>
2015-03-24 22:17:51 +0800

    ex 9.28

--- ch09/ex9_28.cpp
+++ ch09/ex9_28.cpp
@@ -6,7 +6,7 @@
 #include <cassert>
 using namespace std;
 
-bool FindString(forward_list<string>& list, string& str1, string& str2)
+bool FindString(forward_list<string>& list, const string& str1, const string& str2)
 {
         forward_list<string>::iterator pre = list.before_begin();
         forward_list<string>::iterator curr = list.begin();
@@ -35,7 +35,9 @@ int main()
         string str1("huang");
         string str2("xiaosi");
         forward_list<string> l = {"huangyi", "tuzhi", "rubi", "sunletian"};
-        FindString(l, str1, str2);
+        FindString(l, "tuzhi", "awesome");
+        //当FindString中str1用const时才可以采取上面的写法，因为"tuzhi"是一个临时变量，转瞬即逝。
+        //http://blog.csdn.net/zhenwo123/article/details/8031155
         for(auto& i: l)
                 cout<<i<<endl;
 }
