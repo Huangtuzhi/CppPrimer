1176e047f065f8fb3b74fc57089d0baaa806ca50
Huangtuzhi <heying1991@gmail.com>
2015-03-26 11:21:52 +0800

    ex10_3

--- ch10/ex10_11.cpp
+++ ch10/ex10_11.cpp
@@ -9,17 +9,25 @@
 #include <numeric>
 using namespace std;
 
+bool isShorter(const string& s1, const string& s2)
+{
+	return s1.size() < s2.size();
+}
+
 bool elimDups(vector<string>& words)
 {
 	for(auto &i: words)
 		cout << i <<" ";
-	sort(words.begin(), words.end());
+	stable_sort(words.begin(), words.end(), isShorter);
 	auto end_unique = unique(words.begin(), words.end());
 	words.erase(end_unique, words.end());
+	cout<< "\n"<<"Words elimed\n";
 	for(auto &i: words)
 		cout << i <<" ";
 	return true;
 }
+
+
 
 int main()
 {
--- ch10/ex10_9.cpp
+++ ch10/ex10_9.cpp
@@ -16,6 +16,7 @@ bool elimDups(vector<string>& words)
 	sort(words.begin(), words.end());
 	auto end_unique = unique(words.begin(), words.end());
 	words.erase(end_unique, words.end());
+	cout<< "\n"<<"Words elimed\n";
 	for(auto &i: words)
 		cout << i <<" ";
 	return true;
