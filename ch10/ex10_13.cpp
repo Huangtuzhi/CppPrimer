b6e8b6d0804a848e18f748c531f8d3ed4f6d72f2
Huangtuzhi <heying1991@gmail.com>
2015-03-26 14:19:55 +0800

    ex10_13

---
+++ ch10/ex10_13.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <string>
+#include <list>
+#include <iostream>
+#include <forward_list>
+#include <cassert>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+bool Morechars(string str)
+{
+	return str.size() >=5 ? true : false;
+}
+
+int main()
+{
+	vector<string> vec = {"ab", "abc", "abcde", "abcdef", "abcdefgh", "jhkjh", "lkjh", "adf", "ad", "g"};
+	auto r = partition(vec.begin(), vec.end(), Morechars);
+	for(auto i = vec.begin(); i != r; i++)
+		cout << *i << " ";
+}
+ 
