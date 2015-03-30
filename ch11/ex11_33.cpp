13b3b82212384d92ef1f3d1f8829a5de3b50e85d
Huangtuzhi <heying1991@gmail.com>
2015-03-30 09:31:21 +0800

    ex11_33

--- ch11/ex11_33.cpp
+++ ch11/ex11_33.cpp
@@ -19,11 +19,11 @@ map<string, string> buildMap(ifstream& map_file)
 
     while(map_file >> key && getline(map_file, value)){
         if(value.size() > 1)
-            trans_map[key] = value.substr(1);
+            trans_map.insert({key, value.substr(1)});
+            //trans_map[key] = value.substr(1);
         else
             throw runtime_error("no rule for " + key);
     }
-
     return trans_map;
 }
 
@@ -41,10 +41,10 @@ void word_transform(ifstream& map_file, ifstream& input)
     auto trans_map = buildMap(map_file);
     string text;
     while(getline(input, text)){
-        istringstream stream(text);
-        string word;
+        //istringstream stream(text);
+        string word = text;
         bool firstword = true;
-        while(stream >> word){
+        while(word){
             if (firstword)
                 firstword = false;
             else
---
+++ ch12/ex12_2.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+#include <string>
+#include <initializer_list>
+#include <memory>
+#include <exception>
+#include <iostream>
+
+using namespace std;
+
+class StrBob {
+public:
+    typedef vector<string>::size_type size_type;
+    StrBob(): data(make_shared<vector<string>>()) {};
+    StrBob(initializer_list<string> il): data(make_shared<vector<string>> (il)){}
+    size_type size() const { return data->size(); };
+    bool empty() const { return data->empty(); };
+    
+    void push_back(const string& t) { data->push_back(t); }
+    void pop_back(){
+    check(0, "pop_back on empty StrBob");
+    data->pop_back();
+    }
+    
+    string& front(){
+    check(0, "front on empty StrBob");
+    return data->front();
+    }
+
+    string& back(){
+    check(0, "back on empty StrBob");
+    return data->back();
+    }
+
+private:
+    shared_ptr<vector<string>> data;
+    
+    void check(size_type i, const string& msg) const{
+    if (i >= data->size())
+        throw out_of_range(msg);
+    }
+};
+
+int main()
+{
+    StrBob huangyi{"hello", "world", "huangyi"};
+    cout << huangyi.front() << " " << huangyi.back() << endl;
+    huangyi.push_back("I love you");
+    cout << huangyi.front() << " " << huangyi.back() << endl;
+}
